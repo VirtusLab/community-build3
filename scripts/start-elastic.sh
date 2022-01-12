764cbf6337ec4f45063e6a6172c44064ed90e931
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-12 10:45:18 +0100

    Don't try to re-create elastic crds when they already exist

--- scripts/start-elastic.sh
+++ scripts/start-elastic.sh
@@ -4,7 +4,7 @@ set -e
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $scriptDir/utils.sh
 
-kubectl create -f https://download.elastic.co/downloads/eck/1.9.1/crds.yaml
+kubectl create -f https://download.elastic.co/downloads/eck/1.9.1/crds.yaml || true
 kubectl apply -f https://download.elastic.co/downloads/eck/1.9.1/operator.yaml
 
 scbk apply -f k8s/elastic.yaml
