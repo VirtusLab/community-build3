1b8e1ce0e8842b8077e976575bb5fe833a83a122
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-12 10:29:25 +0100

    Check if notifing branch is finishing

--- jenkins/common-lib/vars/getLastLabeledBuildStatus.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String jobName, String label) {
-  def jenkins = jenkins.model.Jenkins.instance
-  def job = jenkins.getItemByFullName(jobName)
-  def builds = job.getBuilds()
- 	def lastLabeledBuild = builds.find { it.getDescription() == label }
-	return lastLabeledBuild?.getResult()?.toString()
-}
---
+++ jenkins/common-lib/vars/isLastLabeledBuildComplete.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def call(String jobName, String label) {
+  def jenkins = jenkins.model.Jenkins.instance
+  def job = jenkins.getItemByFullName(jobName)
+  def builds = job.getBuilds()
+ 	def lastLabeledBuild = builds.find { it.getDescription() == label }
+  def buildResult = lastLabeledBuild?.getResult()
+  
+  // If is not yet complete, mark it as done when is in the last stage
+  // This is done to prevent deadlocks when notifing downstream. 
+  buildResult?.isCompleteBuild() || isInNotifyStage(lastLabeledBuild) 
+}
+
+@NonCPS
+def isInNotifyStage(hudson.model.Run run) {
+  run
+	  ?.getLog(10) // expected message is typically 9th last element in the log lines
+	  ?.stream()
+	  ?.anyMatch{ it.contains("(Notify downstream projects)") }
+}
