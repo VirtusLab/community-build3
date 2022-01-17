abce936004ffc7593fe3b4a0b9eca9fb1dd13497
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-17 13:59:15 +0100

    Bump up used images versions

--- jenkins/seeds/computeBuildPlan.groovy
+++ jenkins/seeds/computeBuildPlan.groovy
@@ -21,7 +21,7 @@ pipeline {
                         spec:
                           containers:
                           - name: coordinator
-                            image: virtuslab/scala-community-build-coordinator:v0.0.1
+                            image: virtuslab/scala-community-build-coordinator:v0.0.3
                             imagePullPolicy: IfNotPresent
                             command:
                             - cat
