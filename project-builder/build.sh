94e42e7223eda57f7ded88d2f09e1ea25a953b00
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-01-24 20:20:04 +0100

    Use sbt.exclude to filter out targets from project

--- project-builder/build.sh
+++ project-builder/build.sh
@@ -9,14 +9,17 @@ fi
 repoDir="$1" # e.g. /tmp/shapeless
 scalaVersion="$2" # e.g. 3.0.1-RC1-bin-COMMUNITY-SNAPSHOT
 version="$3" # e.g. 1.0.2-communityBuild
-targets="$4" # e.g. "com.example%foo com.example%bar"
+unfilteredTargets="$4" # e.g. "com.example%foo com.example%bar"
 export CB_MVN_REPO_URL="$5" # e.g. https://mvn-repo/maven2/2021-05-23_1
 enforcedSbtVersion="$6"
 projectConfig="$7"
 
+targetExcludeFilters=$(echo $projectConfig | jq -r '.sbt.exclude? // ["\".*\""] | join ("|")')
+targets=( $( for target in ${unfilteredTargets[@]} ; do echo $target ; done | grep -E -v "(${targetExcludeFilters})" ) )
+
 echo '##################################'
 echo Scala version: $scalaVersion
-echo Disting version $version for targets: $targets
+echo Disting version $version for ${#targets[@]} targets: ${targets[@]}
 echo Project projectConfig: $projectConfig
 echo '##################################'
 
