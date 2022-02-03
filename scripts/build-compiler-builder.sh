d59b262ad9dd7867289231216dc371e6f36c5024
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-02-03 17:53:48 +0100

    Use java 8 to build Dotty

--- scripts/build-compiler-builder.sh
+++ scripts/build-compiler-builder.sh
@@ -10,7 +10,8 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $scriptDir/utils.sh 
 
 VERSION="$1"
-TAG_NAME=$(buildTag $VERSION 11)
+# Use base image with Java 8
+TAG_NAME=$(buildTag $VERSION 8)
 
 docker build -t virtuslab/scala-community-build-compiler-builder:"$VERSION" \
   --build-arg BASE_IMAGE="virtuslab/scala-community-build-builder-base:$TAG_NAME" $scriptDir/../compiler-builder
