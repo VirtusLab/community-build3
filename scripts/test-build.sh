0a12ec26df25639243078155b18aa9982d88f7f2
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-02-07 14:18:47 +0100

    Increase timeout for building compiler in tests

--- scripts/test-build.sh
+++ scripts/test-build.sh
@@ -4,7 +4,7 @@ set -e
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 testNamespace=scala3-community-build-test
-compilerBuilderTimeout=30m
+compilerBuilderTimeout=60m
 projectBuilderTimeout=5m
 
 export MVN_REPO_KEYSTORE_PASSWORD=$(openssl rand -base64 32)
