49e910f9dbd3d4cacb8fb63effd72086a6bc0116
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-02-07 14:49:38 +0100

    Fix building images

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -12,10 +12,12 @@ javaDefault=11
 javaAccessoryVersions=(8 17)
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-$scriptDir/build-builder-base.sh "$VERSION" "$javaDefault"
-$scriptDir/build-quick.sh "$VERSION" "$javaDefault"
-
 for javaVersion in "${javaAccessoryVersions[@]}"; do
   $scriptDir/build-builder-base.sh "$VERSION" "$javaVersion"
   $scriptDir/build-project-builder.sh "$VERSION" "$javaVersion"
 done
+
+# Compiler builder (build in build-quick) build accessory version images before default one
+$scriptDir/build-builder-base.sh "$VERSION" "$javaDefault"
+$scriptDir/build-quick.sh "$VERSION" "$javaDefault"
+
