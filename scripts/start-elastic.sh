85a7b346708e54d7742c4748c2fee31e97766fb3
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-02-07 14:18:32 +0100

    Revert changed elasticsearch version

--- scripts/start-elastic.sh
+++ scripts/start-elastic.sh
@@ -4,7 +4,6 @@ set -e
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $scriptDir/utils.sh
 
-kubectl create -f https://download.elastic.co/downloads/eck/1.9.1/crds.yaml || true
-kubectl apply -f https://download.elastic.co/downloads/eck/1.9.1/operator.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/1.6.0/all-in-one.yaml
 
 scbk apply -f k8s/elastic.yaml
