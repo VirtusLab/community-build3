35460e85aae106339271e52138e1a56f863bd9ae
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-03-15 16:45:14 +0100

    Use already released version of Scala in test-cli and decrease ammount of scala-cli logs

--- scripts/test-cli.sh
+++ scripts/test-cli.sh
@@ -7,12 +7,11 @@ cd $scriptDir/../cli
 # Installation of scala-cli in the GH actions workflow was not very effective, and might have lead to missing binary on the PATH when executing this script
 
 testNamespace=scala3-community-build-test
-cliRunCmd="run scb-cli.scala --jvm=zulu:17 --java-prop communitybuild.version=test --java-prop communitybuild.local.dir=$scriptDir/.. --quiet -- "
+cliRunCmd="run scb-cli.scala --jvm=11 --java-prop communitybuild.version=test --java-prop communitybuild.local.dir=$scriptDir/.. --quiet -- "
 commonOpts="--namespace=$testNamespace --keepCluster --keepMavenRepo --noRedirectLogs"
 sbtProject=typelevel/shapeless-3
 millProject=com-lihaoyi/os-lib
-# It's should actually build 3.1.1 release
-scalaVersion=3.1.1-bin-d0e5cc45da47bedbbce6f6430052ced91c941a08-COMMUNITY-BUILD
+scalaVersion=3.1.1
 
 echo "Test sbt custom build in minikube"
 scala-cli $cliRunCmd run $sbtProject $scalaVersion $commonOpts
