808a7ef7e737ed34d47ca0cf38f070bedf4efc5f
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-03-15 12:24:03 +0100

    Use cache to build docker images

--- scripts/build-mvn-repo.sh
+++ scripts/build-mvn-repo.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $# -ne 1 ]; then 
+if [ $# -ne 1 ]; then
   echo "Wrong number of script arguments"
   exit 1
 fi
 
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
 TAG_NAME="$1"
+CACHE_FROM_TAG="$PREV_CB_VERSION"
 
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+imageName=virtuslab/scala-community-build-mvn-repo
 
-docker build -t virtuslab/scala-community-build-mvn-repo:"$TAG_NAME" $scriptDir/../mvn-repo
+docker pull $imageName:$CACHE_FROM_TAG || true
+docker build \
+  -t "$imageName:$TAG_NAME" \
+  --cache-from "$imageName:$CACHE_FROM_TAG" \
+  $scriptDir/../mvn-repo
