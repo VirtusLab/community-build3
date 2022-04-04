6e35ffd2ee6429d70dd7306255bdfb31702ed80f
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-04-04 11:23:13 +0200

    Configure ES indexes on statup

--- scripts/configure-elasticsearch.sh
+++ scripts/configure-elasticsearch.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
-set -x 
+
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $scriptDir/utils.sh
 
 username="elastic"
 password=$(scbk get secret community-build-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 
+scbk wait po/community-build-es-default-0  --for=condition=Ready
 scbk exec community-build-es-default-0 -- \
   curl -k --user ${username}:${password} \
   -X PUT "https://localhost:9200/project-build-summary?pretty" \
