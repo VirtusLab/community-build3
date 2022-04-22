9aef898ab42247512439cddfa2b4ffe8d287eeae
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-04-22 14:35:02 +0200

    Set Jenkins debug flags

--- scripts/start-jenkins.sh
+++ scripts/start-jenkins.sh
@@ -33,21 +33,23 @@ fi
 HELM_EXPERIMENTAL_OCI=1 helm registry login operatorservice.azurecr.io -u "$CB_DOCKER_USERNAME" -p "$CB_DOCKER_PASSWORD"
 
 scbk apply -f $scriptDir/../k8s/auth/authz-matrix.yaml 
+scbk apply -f $scriptDir/../k8s/jenkins-priority.yaml
 scbk create configmap jenkins-seed-jobs --from-file=$scriptDir/../jenkins/seeds --dry-run=client -o yaml | scbk apply -f -
 scbk create configmap jenkins-common-lib-vars --from-file=$scriptDir/../jenkins/common-lib/vars --dry-run=client -o yaml | scbk apply -f -
 scbk create configmap jenkins-build-configs --from-file=$scriptDir/../env/prod/config --dry-run=client -o yaml | scbk apply -f -
 scbk create configmap jenkins-build-scripts --from-file=$scriptDir/../jenkins/scripts --dry-run=client -o yaml | scbk apply -f -
-scbk get secret ${REMOTE_CREDS_SECRET} 2>/dev/null || \
+scbk get secret ${REMOTE_CREDS_SECRET} 2>/dev/null >/dev/null || \
   scbk create secret generic ${REMOTE_CREDS_SECRET} --from-literal=runbuild-token="${CB_BUILD_TOKEN}"
 
 jenkinsClientId=$(scbk get secret/jenkins-github-oauth-secret -o 'jsonpath={.data.clientID}' | base64 -d)
 
+# Make sure env ids starts from env count set in jenkins.yaml
 HELM_EXPERIMENTAL_OCI=1 helm --namespace="$CB_K8S_NAMESPACE" \
-  install jenkins oci://operatorservice.azurecr.io/charts/op-svc-jenkins-crs --version 0.3.1 -f k8s/jenkins.yaml \
-  --set 'jenkins.podSpec.jenkinsController.env[0].name'=BUILD_CRON_TRIGGER \
-  --set 'jenkins.podSpec.jenkinsController.env[0].value'="$CB_BUILD_CRON_TRIGGER" \
-  --set 'jenkins.podSpec.jenkinsController.env[1].name'=BUILD_TOKEN \
-  --set 'jenkins.podSpec.jenkinsController.env[1].valueFrom.secretKeyRef.name'="$REMOTE_CREDS_SECRET" \
-  --set 'jenkins.podSpec.jenkinsController.env[1].valueFrom.secretKeyRef.key'="runbuild-token" \
+  upgrade jenkins oci://operatorservice.azurecr.io/charts/op-svc-jenkins-crs --install --version 0.3.1 -f k8s/jenkins.yaml \
+  --set 'jenkins.podSpec.jenkinsController.env[1].name'=BUILD_CRON_TRIGGER \
+  --set 'jenkins.podSpec.jenkinsController.env[1].value'="$CB_BUILD_CRON_TRIGGER" \
+  --set 'jenkins.podSpec.jenkinsController.env[2].name'=BUILD_TOKEN \
+  --set 'jenkins.podSpec.jenkinsController.env[2].valueFrom.secretKeyRef.name'="$REMOTE_CREDS_SECRET" \
+  --set 'jenkins.podSpec.jenkinsController.env[2].valueFrom.secretKeyRef.key'="runbuild-token" \
   --set 'jenkinsAuthentication.githubOAuth.clientID'="${jenkinsClientId}" \
   --set 'jenkinsAuthentication.githubOAuth.clientSecretRef.namespace'="$CB_K8S_NAMESPACE"
