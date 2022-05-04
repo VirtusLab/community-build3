67fd57b62b40d6d932cf5059206ffa6a1a551750
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-05-04 15:26:24 +0200

    Make sure that build lable is not null

--- jenkins/common-lib/vars/isLastLabeledBuildFinished.groovy
+++ jenkins/common-lib/vars/isLastLabeledBuildFinished.groovy
@@ -4,8 +4,8 @@ def call(String jobName, String label) {
   def jenkins = jenkins.model.Jenkins.instance
   def job = jenkins.getItemByFullName(jobName)
   def builds = job.getBuilds()
-  def trimmedLabel = label.trim()
- 	def buildsWithLabel = builds.findAll { it.getDescription().trim() == trimmedLabel }
+  def trimmedLabel = label?.trim()
+ 	def buildsWithLabel = builds.findAll { it.getDescription() && it.getDescription()?.trim() == trimmedLabel }
   if(!buildsWithLabel) return false
   else {
     def last = buildsWithLabel.last()
