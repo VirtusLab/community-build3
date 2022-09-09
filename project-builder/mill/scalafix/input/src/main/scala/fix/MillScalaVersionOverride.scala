79973327d8cb2bb5d03fe18b4eb1e59f83e3f6e8
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-09-10 00:25:40 +0200

    Don't override Scala2 variables/literals in mill

--- project-builder/mill/scalafix/input/src/main/scala/fix/MillScalaVersionOverride.scala
+++ project-builder/mill/scalafix/input/src/main/scala/fix/MillScalaVersionOverride.scala
@@ -22,9 +22,30 @@ object MillScalaVersionOverride {
   class moduleDef2 extends moduleDef {
     override val scalaVersion: T[String] = MillScalaVersionOverride.scala3Version
   }
+  class moduleDef3 extends moduleDef {
+    override val scalaVersion: T[String] = "3.2.1-RC1"
+  }
+  class moduleDef4 extends moduleDef {
+    override val scalaVersion = sv
+    def sv = scala3
+    def scala3 = "3.1.3"
+  }
 
   class otherDef {
     def scalaVersion() = "1.2.3"
+  }
+
+  object unchanged{
+    object Scala{def scala213 = "2.13.8"}
+    import Scala._
+    class case1 { def scalaVersion: T[String] = "2.13.8" }
+    class case2 { def scalaVersion: T[String] = scala213 }
+    class case3 { def scalaVersion: T[String] = Scala.scala213 }
+    class case4 {
+      val scalaVersion = sv
+      def sv = scala213
+      def scala213 = "2.13.8"
+    }
   }
 
   val snippet = s"""
--- project-builder/mill/scalafix/output/src/main/scala/fix/MillScalaVersionOverride.scala
+++ project-builder/mill/scalafix/output/src/main/scala/fix/MillScalaVersionOverride.scala
@@ -1,7 +1,7 @@
 package fix
 
 object MillScalaVersionOverride {
-  def scala3Version = "3.1.1"
+  def scala3Version = mill.T("3.1.2-RC2-bin-cb00abcdef123456789-COMMUNITY-BUILD")
   object mill{
     import scala.language.implicitConversions
     trait T[U]
@@ -19,9 +19,30 @@ object MillScalaVersionOverride {
   class moduleDef2 extends moduleDef {
     override val scalaVersion: T[String] = mill.T("3.1.2-RC2-bin-cb00abcdef123456789-COMMUNITY-BUILD")
   }
+  class moduleDef3 extends moduleDef {
+    override val scalaVersion: T[String] = mill.T("3.1.2-RC2-bin-cb00abcdef123456789-COMMUNITY-BUILD")
+  }
+  class moduleDef4 extends moduleDef {
+    override val scalaVersion = sv
+    def sv = scala3
+    def scala3 = mill.T("3.1.2-RC2-bin-cb00abcdef123456789-COMMUNITY-BUILD")
+  }
 
   class otherDef {
     def scalaVersion() = "1.2.3"
+  }
+
+  object unchanged{
+    object Scala{def scala213 = "2.13.8"}
+    import Scala._
+    class case1 { def scalaVersion: T[String] = "2.13.8" }
+    class case2 { def scalaVersion: T[String] = scala213 }
+    class case3 { def scalaVersion: T[String] = Scala.scala213 }
+    class case4 {
+      val scalaVersion = sv
+      def sv = scala213
+      def scala213 = "2.13.8"
+    }
   }
 
   val snippet = s"""
