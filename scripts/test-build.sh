4f65c76a10f01c7a7ce26b8b582e317fa82f03d9
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-09-27 21:53:20 +0200

    Bump version to 0.1.1

--- scripts/test-build.sh
+++ scripts/test-build.sh
@@ -9,6 +9,10 @@ projectBuilderTimeout=5m
 kubectl delete namespace $testNamespace --ignore-not-found=true
 kubectl create namespace $testNamespace
 
+CB_VERSION="test" \
+  CB_K8S_NAMESPACE="${testNamespace}" \
+  $scriptDir/start-mvn-repo.sh
+
 function projectBuilderFailed() {
   jobName="$1"
   echo "Failed to publish the community project"
