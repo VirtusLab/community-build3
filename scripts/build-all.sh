3bf019ec84ce23355ad551053518a584b53722a0
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-09-27 17:52:23 +0200

    Don't build accessory images in CI

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -13,10 +13,14 @@ javaDefault=11
 javaAccessoryVersions=(8 17)
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-for javaVersion in "${javaAccessoryVersions[@]}"; do
-  $scriptDir/build-builder-base.sh "$VERSION" "$javaVersion"
-  $scriptDir/build-project-builder.sh "$VERSION" "$javaVersion"
-done
+if [[ $VERSION != "test" ]]; then
+  for javaVersion in "${javaAccessoryVersions[@]}"; do
+    $scriptDir/build-builder-base.sh "$VERSION" "$javaVersion"
+    $scriptDir/build-project-builder.sh "$VERSION" "$javaVersion"
+  done
+else 
+  echo "Skipping accessory jdk images builds"
+fi
 
 # Compiler builder (build in build-quick) build accessory version images before default one
 $scriptDir/build-builder-base.sh "$VERSION" "$javaDefault"
