780c3025e99f50931785316949bec5cb7ce6e172
Wojciech Mazur <wmazur@virtuslab.com>
2023-07-14 15:33:11 +0200

    Upgrade version to v0.2.6

--- scripts/bisect.scala
+++ scripts/bisect.scala
@@ -9,7 +9,7 @@ import java.nio.file.attribute.PosixFilePermissions
 import java.nio.charset.StandardCharsets
 import java.nio.file._
 
-val communityBuildVersion = "v0.2.5"
+val communityBuildVersion = "v0.2.6"
 
 @main def run(args: String*): Unit =
   val config = scopt.OParser
