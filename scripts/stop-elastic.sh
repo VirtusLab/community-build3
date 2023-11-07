f843563da3237a764811035cc8160da5571bb826
Wojciech Mazur <wmazur@virtuslab.com>
2023-11-07 17:12:22 +0100

    Upgrade infra setup

--- scripts/stop-elastic.sh
+++ scripts/stop-elastic.sh
@@ -5,3 +5,5 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $scriptDir/utils.sh
 
 scbk delete -f $scriptDir/../k8s/elastic.yaml
+kubectl delete -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml 
+kubectl delete -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
