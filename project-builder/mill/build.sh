577a76fe97fc687bdc23b1fb25521968cb7062e5
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-02 21:40:23 +0100

    Restored filtered out mill 0.11 projects and other which should now pass

--- project-builder/mill/build.sh
+++ project-builder/mill/build.sh
@@ -52,5 +52,8 @@ function tryBuild() {
   $mill ${millSettings[@]} runCommunityBuild "$scalaVersion" "${projectConfig}" "${targets[@]}"
 }
 
+rm ../build-summary.txt && touch ../build-summary.txt
+cat ../build-summary.txt
 # Retry only if previous build failed to start
+export MILL_VERSION=$millVersion
 tryBuild mill || ([[ -f ./mill && ! -s ../build-summary.txt ]] && tryBuild ./mill)
