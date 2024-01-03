74e81914689487bee1a9426675368c01c65ddc6a
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-03 22:21:57 +0100

    Upgrade version to 0.3.0

--- scripts/bisect.scala
+++ scripts/bisect.scala
@@ -9,7 +9,7 @@ import java.nio.file.attribute.PosixFilePermissions
 import java.nio.charset.StandardCharsets
 import java.nio.file._
 
-val communityBuildVersion = "v0.2.8"
+val communityBuildVersion = "v0.3.0"
 
 @main def run(args: String*): Unit =
   val config = scopt.OParser
--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -7,7 +7,7 @@ if [ $# -ne 1 ]; then
 fi
 
 VERSION="$1"
-export PREV_CB_VERSION="v0.2.7"
+export PREV_CB_VERSION="v0.2.8"
 
 javaDefault=11
 javaAccessoryVersions=(8 17 21)
