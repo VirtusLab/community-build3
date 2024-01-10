3b6118059bb195b19e505d72a0c098f1ab23cd1b
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-10 14:01:51 +0100

    Allow to specify fixed source version to prevent overriding -source

--- project-builder/build-revision.sh
+++ project-builder/build-revision.sh
@@ -29,7 +29,15 @@ scalaBinaryVersionMajor=`echo $scalaVersion | cut -d . -f 1`
 scalaBinaryVersionMinor=`echo $scalaVersion | cut -d . -f 2`
 echo "Scala binary version found: $scalaBinaryVersion"
 
-commonAppendScalacOptions="-source:$scalaBinaryVersion-migration,-Wconf:msg=can be rewritten automatically under -rewrite -source $scalaBinaryVersion-migration:s"
+sourceVersion=`echo $projectConfig | jq -r '.sourceVersion // ""'`
+if [[ -z "$sourceVersion" ]]; then
+  sourceVersion="$scalaBinaryVersion-migration"
+  echo "Implicitly using source version $sourceVersion"
+else 
+  echo "Using configured source version: $sourceVersion"
+fi
+
+commonAppendScalacOptions="-source:$sourceVersion,-Wconf:msg=can be rewritten automatically under:s"
 commonRemoveScalacOptions="-deprecation,-feature,-Xfatal-warnings,-Werror,MATCH:.*-Wconf.*any:e,-migration,"
 echo "Would try to apply common scalacOption (best-effort, sbt/mill only):"
 echo "Append: $commonAppendScalacOptions"
