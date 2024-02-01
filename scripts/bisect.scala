25e7907acfefbdf1713bbf7c471928d1ae46f4c8
Wojciech Mazur <wmazur@virtuslab.com>
2024-02-02 00:29:21 +0100

    Set version to 0.3.5

--- scripts/bisect.scala
+++ scripts/bisect.scala
@@ -9,7 +9,7 @@ import java.nio.file.attribute.PosixFilePermissions
 import java.nio.charset.StandardCharsets
 import java.nio.file._
 
-val communityBuildVersion = "v0.3.4"
+val communityBuildVersion = "v0.3.5"
 
 @main def run(args: String*): Unit =
   val config = scopt.OParser
