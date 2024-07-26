7cedf67e4642afe112cca142645eed3d371c69d1
Wojciech Mazur <wmazur@virtuslab.com>
2024-07-26 18:57:21 +0200

    Set version to 0.3.14

--- cli/scb-cli.scala
+++ cli/scb-cli.scala
@@ -31,7 +31,7 @@ class FailedProjectException(msg: String)
     with NoStackTrace
 
 val communityBuildVersion =
-  sys.props.getOrElse("communitybuild.version", "v0.3.12")
+  sys.props.getOrElse("communitybuild.version", "v0.3.14")
 private val CBRepoName = "VirtusLab/community-build3"
 val projectBuilderUrl =
   s"https://raw.githubusercontent.com/$CBRepoName/master/project-builder"
