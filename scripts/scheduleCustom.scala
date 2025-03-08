30f29c69afcf3e89465ad6009fcc8ba902211d69
Wojciech Mazur <wmazur@virtuslab.com>
2025-03-08 11:35:56 +0100

    Allow to specify source version when scheduling custom build

--- scripts/scheduleCustom.scala
+++ scripts/scheduleCustom.scala
@@ -6,9 +6,19 @@ import scala.sys.process.*
 
 @main def scheduleCustom(repository: String, branch: String, args: String*) = 
   var executeTests = false 
+  var sourceVersion = Option.empty[String]
+  var scalacOptions = List.empty[String]
   args.foreach{
     case s"--executeTests" => executeTests = true
-    
+    case s"--sourceVersion=${version}" => 
+      version match {
+        case s"$major.$minor" => // ok
+        case s"$major.$minor-migration" => // ok
+        case "future" | "future-migration" => // ok 
+        case _ => sys.error(s"Invalid sourceVersion $version")
+      }
+      scalacOptions ::= s"REQUIRE:-source:$version"
+      sourceVersion = Some(version)
   }
   repository match {
     case s"$repo/$project" => println(s"Build $repo/$project branch: $branch")
@@ -18,12 +28,14 @@ import scala.sys.process.*
 
   for build <- Seq("A", "B")
   do {   
+    val sourceVersionAttr = sourceVersion.map("-source:" + _).getOrElse("")
     val task =   
     s"""gh workflow run .github/workflows/buildExecuteCustom-$build.yaml 
-    | -f build-name=${repository}:${branch}:$date
+    | -f build-name=${repository}:${branch}${sourceVersionAttr}:$date
     | -f execute-tests=${executeTests}
     | -f repository-url=${repository}
     | -f repository-branch=${branch}
+    | -f extra-scalac-options=${scalacOptions.mkString(",")}
     |""".stripMargin
     
     println(s"Eval: $task")
