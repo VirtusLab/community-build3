4525c2a90986a9ade4eb5cdf5b46aa61ccaa96d9
Wojciech Mazur <wmazur@virtuslab.com>
2025-07-11 19:52:57 +0200

    Update coordinator deps

--- coordinator/src/main/scala/build.scala
+++ coordinator/src/main/scala/build.scala
@@ -1,10 +1,9 @@
-//> using scala "3.7.0-RC1"
+//> using scala "3.7"
 //> using jvm 17
-//> using dep "org.jsoup:jsoup:1.19.1"
+//> using dep "org.jsoup:jsoup:1.21.1"
 //> using dep "org.json4s::json4s-native:4.0.7"
 //> using dep "org.json4s::json4s-ext:4.0.7"
-//> using dep "com.github.pureconfig::pureconfig-core:0.17.8"
-//> using dep com.softwaremill.sttp.client4::core:4.0.0-M17
+//> using dep "com.github.pureconfig::pureconfig-core:0.17.9"
 //> using toolkit 0.7.0
 
 //> using resourceDir "../resources"
