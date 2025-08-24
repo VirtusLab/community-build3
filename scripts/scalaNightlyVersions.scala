51c78f55564958d820fc986aea46c81e1bcb992c
Wojciech Mazur <wmazur@virtuslab.com>
2025-08-24 22:16:43 +0200

    Fix detection of nightly versions

--- scripts/scalaNightlyVersions.scala
+++ scripts/scalaNightlyVersions.scala
@@ -13,10 +13,20 @@ import java.time.LocalDate
     case s"--max=$version" => maxScalaVersion = Some(version) 
   }
   
-  val scalaVersions = {
-    val regex = raw"<version>(.+-bin-\d{8}-\w{7}-NIGHTLY)</version>".r
-    val xml = io.Source.fromURL("https://repo1.maven.org/maven2/org/scala-lang/scala3-compiler_3/maven-metadata.xml")
-    regex.findAllMatchIn(xml.mkString).map(_.group(1)).filter(_ != null).toList.sorted
+  val scalaVersions: List[String] = {
+    Seq(
+      // 3.8.0-
+      "https://repo1.maven.org/maven2/org/scala-lang/scala3-compiler_3/maven-metadata.xml",
+      // 3.8.0+
+      "https://repo.scala-lang.org/artifactory/maven-nightlies/org/scala-lang/scala3-compiler_3/maven-metadata.xml",     
+    ).map(io.Source.fromURL(_).mkString)
+    .flatMap: metadataXml =>
+      raw"<version>(.+-bin-\d{8}-\w{7}-NIGHTLY)</version>".r
+      .findAllMatchIn(metadataXml)
+      .map(_.group(1))
+      .filter(_ != null).toList
+    .distinct.sorted
+    .toList
   }
   
   val filters = Seq[List[String] => List[String]](
