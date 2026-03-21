1fdda20b894fecab3e4e6508c0d08d41905360da
Wojciech Mazur <wmazur@virtuslab.com>
2026-03-21 17:12:35 +0100

    coordinator: update dependencies

--- coordinator/src/main/scala/build.scala
+++ coordinator/src/main/scala/build.scala
@@ -1,10 +1,10 @@
-//> using scala "3.7"
-//> using jvm 17
-//> using dep "org.jsoup:jsoup:1.21.2"
+//> using scala "3.8"
+//> using jvm 25
+//> using dep "org.jsoup:jsoup:1.22.1"
 //> using dep "org.json4s::json4s-native:4.0.7"
 //> using dep "org.json4s::json4s-ext:4.0.7"
-//> using dep "com.github.pureconfig::pureconfig-core:0.17.9"
-//> using toolkit 0.7.0
+//> using dep "com.github.pureconfig::pureconfig-core:0.17.10"
+//> using toolkit 0.9.1
 
 //> using resourceDir "../resources"
 //> using options -Wunused:all -deprecation
