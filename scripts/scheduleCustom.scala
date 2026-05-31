3e0ffdf7cc2e2bbe4b4374587c2caa19e13078d3
Wojciech Mazur <wmazur@virtuslab.com>
2026-05-31 23:00:33 +0200

    [scripts] Make scheduleCusotm take url

--- scripts/scheduleCustom.scala
+++ scripts/scheduleCustom.scala
@@ -1,10 +1,20 @@
 #!/usr/bin/env scala shebang
 
-import java.nio.file.{Path, Files}
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 import scala.sys.process.*
 
-@main def scheduleCustom(repository: String, branch: String, args: String*) = 
+def parseGitHubBranchUrl(url: String): (repository: String, branch: String) =
+  val pattern = """https?://(?:www\.)?github\.com/([^/]+)/([^/]+)/tree/(.+)""".r
+  url.stripSuffix("/") match
+    case pattern(owner, repo, branch) =>
+      (s"$owner/$repo", URLDecoder.decode(branch, StandardCharsets.UTF_8))
+    case _ =>
+      sys.error(s"Invalid GitHub branch URL: $url (expected https://github.com/owner/repo/tree/branch)")
+
+@main def scheduleCustom(githubUrl: String, args: String*) =
+  val (repository, branch) = parseGitHubBranchUrl(githubUrl)
   var executeTests = false 
   var pushToGHPages = false
   var sourceVersion = Option.empty[String]
@@ -22,10 +32,7 @@ import scala.sys.process.*
       scalacOptions ::= s"REQUIRE:-source:$version"
       sourceVersion = Some(version)
   }
-  repository match {
-    case s"$repo/$project" => println(s"Build $repo/$project branch: $branch")
-    case _ => sys.error(s"Invalid repository: $repository")
-  }
+  println(s"Build $repository branch: $branch")
   val date = LocalDate.now()
 
   for build <- Seq("A", "B")
