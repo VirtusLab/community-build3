2e30216b4105db87d84e3b7d3ea95d6d0c7dcccf
Wojciech Mazur <wojciech.mazur95@gmail.com>
2026-07-19 20:06:52 +0200

    Increase build plan timeout to 3h

--- coordinator/src/main/scala/buildPlan.scala
+++ coordinator/src/main/scala/buildPlan.scala
@@ -158,7 +158,7 @@ val ForReproducer = sys.props.contains("opencb.coordinator.reproducer-mode")
         }
       }
   }
-  try Await.result(task, 90.minute)
+  try Await.result(task, 180.minute)
   catch {
     case ex: Throwable =>
       ex.printStackTrace()
